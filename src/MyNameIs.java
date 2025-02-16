ec788b6d0f605abe33581de6720ca6b0e13d4b2d
MatheusSantos285 <santos0508matheus@outlook.com>
2025-02-16 00:46:01 -0300

    Código dos exercícios 7, 8

---
+++ src/MyNameIs.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class MyNameIs {
+    public static void main(String[] args) {
+
+        String name = "Matheus";
+        LocalDate current = LocalDate.now();
+
+        System.out.println("Hello, World!");
+        System.out.println("My name is " + name + "!");
+        System.out.println("Date: " + current);
+    }
+}
---
+++ src/VariaveisTiposDados.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+
+public class VariaveisTiposDados {
+    public static void main(String[] args) {
+
+        String name = "Matheus";
+        int number = 1;
+        double pi = 3.14;
+        boolean isActive = true;
+        char caractere = 'M';
+
+        System.out.println(name);
+        System.out.println(number);
+        System.out.println(pi);
+        System.out.println(isActive);
+        System.out.println(caractere);
+    }
+}
